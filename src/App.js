c64c5d38906b662451a59002199846b9d4558b4b
Sondre <89258011+h600853@users.noreply.github.com>
2024-02-15 17:36:05 +0100

    changes

--- src/App.js
+++ src/App.js
@@ -4,12 +4,14 @@ import Header from "./components/Header";
 import { Stack } from "@mui/material";
 import Description from "./components/Description";
 import ImageComponent from "./components/ImageComponent";
+import ToDoList from "./components/ToDoList";
 
 export default function App() {
   return (
     <Container maxWidth="lg">
       <Header />
       <Stack direction="column" spacing={2} mt={2}>
+        <ToDoList />
         <Description />
         <ImageComponent />
       </Stack>
--- src/components/Header.js
+++ src/components/Header.js
@@ -6,7 +6,7 @@ export default function Header() {
     <AppBar position="static">
       <Toolbar>
         <Typography variant="h6" noWrap>
-          Daily
+          React kurs
         </Typography>
       </Toolbar>
     </AppBar>
